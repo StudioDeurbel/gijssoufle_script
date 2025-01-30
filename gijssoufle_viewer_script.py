708a4f6537398faae50f6d48ec693fb7607f0635
StudioDeurbel <nikenaukjeandries@gmail.com>
2025-01-30 16:51:56 +0100

    Add files via upload

---
+++ gijssoufle_viewer_script.py
@@ -0,0 +1,7 @@
+viewer_input = input("Voer hier een viewer in: ")
+if viewer_input == "Daysecond":
+    print("Daysecond, Species: Brawler")
+if viewer_input == "peos":
+    print("peos, Species: Peos")
+if viewer_input == "Gijssoufle":
+    print("Gijssoufle, Species: Half Mens Half Kaassouflé")
---
+++ gijssouflescript.py
@@ -0,0 +1,5 @@
+gijssoufle_input = input("Is gijssoufle de beste youtuber? ")
+if gijssoufle_input == "ja":
+    print("Goedzo!")
+if gijssoufle_input == "nee":
+    print("FOUT BRO.")
